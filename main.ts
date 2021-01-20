294c0f15faa7f136a8b76cc40a21b3f2def95205
Casper Bekmand <casper.bekmand@hotmail.com>
2021-01-20 22:51:35 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,27 @@
-// Button Press to cycle through list of object and display them
-input.onButtonPressed(Button.A, function () {
-    for (let value of list) {
-        if (value == 0) {
-            basic.showIcon(IconNames.Duck)
-            CurrentSelected = 0
-        }
-        if (value == 1) {
-            basic.showIcon(IconNames.Heart)
-            CurrentSelected = 1
-        }
+let CurrentSelected = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        CurrentSelected = 0
+    } else if (receivedNumber == 1) {
+        CurrentSelected = 0
     }
 })
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == "Duck") {
+input.onButtonPressed(Button.A, function () {
+    CurrentSelected = 0
+})
+// Button Press to cycle through list of object and display them
+input.onButtonPressed(Button.AB, function () {
+    if (CurrentSelected == 0) {
         basic.showIcon(IconNames.Duck)
-    } else if (receivedString == "Heart") {
+    }
+    if (CurrentSelected == 1) {
         basic.showIcon(IconNames.Heart)
     }
+})
+input.onButtonPressed(Button.B, function () {
+    CurrentSelected = 1
 })
 input.onGesture(Gesture.Shake, function () {
     basic.clearScreen()
     radio.sendString("" + (CurrentSelected))
 })
-let CurrentSelected = 0
-let list: number[] = []
-list = [0, 1]
-CurrentSelected = 0
